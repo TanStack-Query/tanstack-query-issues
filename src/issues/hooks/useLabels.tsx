a585bde381d73bbef7cfc9be475a1174f32c3eb1
= <=>
2024-08-10 12:36:56 +0200

    Load issues and interface

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -7,42 +7,6 @@ export const useLabels = () => {
     queryKey: ["labels"],
     queryFn: getLabels,
     staleTime: 1000 * 60 * 60,
-    // placeholderData: [
-    //   {
-    //     id: 739777675,
-    //     node_id: "MDU6TGFiZWw3Mzk3Nzc2NzU=",
-    //     url: "https://api.github.com/repos/facebook/react/labels/Component:%20Component%20API",
-    //     name: "Component: Component API",
-    //     color: "d4c5f9",
-    //     default: false,
-    //   } satisfies GitHubLabel,
-    //   {
-    //     id: 1155972012,
-    //     node_id: "MDU6TGFiZWwxMTU1OTcyMDEy",
-    //     url: "https://api.github.com/repos/facebook/react/labels/Component:%20Scheduler",
-    //     name: "Component: Scheduler",
-    //     color: "9de8f9",
-    //     default: false,
-    //   } satisfies GitHubLabel,
-    // ],
-    // initialData: [
-    //   {
-    //     id: 739777675,
-    //     node_id: "MDU6TGFiZWw3Mzk3Nzc2NzU=",
-    //     url: "https://api.github.com/repos/facebook/react/labels/Component:%20Component%20API",
-    //     name: "Component: Component API",
-    //     color: "d4c5f9",
-    //     default: false,
-    //   } satisfies GitHubLabel,
-    //   {
-    //     id: 1155972012,
-    //     node_id: "MDU6TGFiZWwxMTU1OTcyMDEy",
-    //     url: "https://api.github.com/repos/facebook/react/labels/Component:%20Scheduler",
-    //     name: "Component: Scheduler",
-    //     color: "9de8f9",
-    //     default: false,
-    //   } satisfies GitHubLabel,
-    // ],
   });
 
   return {
